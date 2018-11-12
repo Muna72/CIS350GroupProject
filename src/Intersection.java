caebb0a27b285dc5600f69c72fcfbf2f0a80c2a6
Muna Gigowski <munagigowski@gmail.com>
2018-11-12 14:10:31 -0500

    Remove unneeded code in intersection class

--- src/Intersection.java
+++ src/Intersection.java
@@ -16,28 +16,6 @@ public class Intersection
     public LinkedList<Vehicle>[] entryPoint = new LinkedList[8];
 
     /**
-     * Holds the number of vehicles that have passed through
-     * a lane in a specified direction; corresponds to entryPoint[]
-     */
-    private int[] entryPointCounter = new int[8];
-
-    /**
-     * Holds the number of vehicles that have passed through the intersection
-     */
-    private int interCounter = 0;
-
-
-    /**
-     * Holds the number type of vehicle
-     */
-    private String type;
-
-    /**
-     * Used to generate pseudo-random numbers
-     */
-    private Random rand = new Random();
-
-    /**
      * Default class constructor, initializing the linked lists and counters
      */
     public Intersection()
@@ -50,44 +28,6 @@ public class Intersection
         entryPoint[5] = new LinkedList<Vehicle>();
         entryPoint[6] = new LinkedList<Vehicle>();
         entryPoint[7] = new LinkedList<Vehicle>();
-
-        entryPointCounter[0] = 0;
-        entryPointCounter[1] = 0;
-        entryPointCounter[2] = 0;
-        entryPointCounter[3] = 0;
-        entryPointCounter[4] = 0;
-        entryPointCounter[5] = 0;
-        entryPointCounter[6] = 0;
-        entryPointCounter[7] = 0;
-
-    }
-
-    /**
-     * Intersection class constructor
-     * Initializes each entry point with a linked list with a specified
-     * number of random vehicles
-     *
-     * @param horizCycleCars
-     * @param horizCycleTime
-     * @param vertCycleCars
-     * @param vertCycleTime
-     * @param type
-     */
-    public Intersection(int horizCycleTime, int horizCycleCars, int vertCycleTime,
-                        int vertCycleCars, String type)
-    {
-        entryPoint[0] = new LinkedList<Vehicle>();
-        entryPoint[1] = new LinkedList<Vehicle>();
-        entryPoint[2] = new LinkedList<Vehicle>();
-        entryPoint[3] = new LinkedList<Vehicle>();
-
-
-        entryPointCounter[0] = 0;
-        entryPointCounter[1] = 0;
-        entryPointCounter[2] = 0;
-        entryPointCounter[3] = 0;
-
-        this.type = type;
     }
 
     /**
@@ -102,32 +42,5 @@ public class Intersection
         entryPoint[5].clear();
         entryPoint[6].clear();
         entryPoint[7].clear();
-    }
-
-    /**
-     * Sets the given linked list to a lane within the intersection
-     *
-     * @param list A linked list of vehicles lined up at the intersection
-     * @param loc The lane that the linked list will be assigned to
-     */
-    public void setLocation(LinkedList<Vehicle> list, Location loc) {
-
-    }
-    /**
-     * Sets the vehicle type
-     *
-     * @param t The vehicle type
-     */
-    public void setType(String t) {
-        type = t;
-    }
-
-    /**
-     * Returns the type of the vehicle
-     *
-     * @return type The type of vehicle
-     */
-    public String getType() {
-        return type;
     }
 }
