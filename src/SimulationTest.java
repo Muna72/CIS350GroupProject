af3ee92bf7a9649995d3517e8a9b7be6753a29c0
trangun <42783313+trangun@users.noreply.github.com>
2018-11-13 21:36:30 -0500

    Add files via upload
    partial test

---
+++ src/SimulationTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimulationTest {
+    private Simulation s;
+    @Test
+    public void testConstructor() {
+        s = new Simulation(20.0, 10.0, 500);
+        assertEquals(s.getAvgVehicleSpeed(), 50,0);
+        s = new Simulation(20.0, 10.0, 700);
+        assertEquals(s.getAvgVehicleSpeed(), 35.5,0);
+        s = new Simulation(20.0, 10.0, 800);
+        assertEquals(s.getAvgVehicleSpeed(), 31.25,0);
+        s = new Simulation(20.0, 10.0, 1000);
+        assertEquals(s.getAvgVehicleSpeed(), 25,0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithErrors1() {
+        new Simulation(-1,0,500);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithErrors2() {
+        new Simulation(0,2,-6);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithErrors3() {
+        new Simulation(0,-2,500);
+    }
+
+
+    @Test (expected = NullPointerException.class)
+    public void testReset(){
+        s.reset();
+        assertEquals(s.getSimTimeLeft(),0,0);
+
+
+    }
+}
