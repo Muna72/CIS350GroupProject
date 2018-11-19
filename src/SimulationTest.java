9496b7512ebec09f6230df766763786081a0036a
kerrbri <33265130+kerrbri@users.noreply.github.com>
2018-11-19 14:31:38 -0500

    tests for: setPath, placeVehicle, addVehicle

--- src/SimulationTest.java
+++ src/SimulationTest.java
@@ -49,5 +49,157 @@ public class SimulationTest {
         assertEquals(s.getNumOfAccidents(),0,0);
         assertEquals(s.getUserThruTime(),0,0);
     }
+    
+    @Test
+    public void testSetPath() {
+        FakeRandom r = new FakeRandom(0);
+        s = new Simulation(100, 100000, 300, r);
+        Vehicle v = new Car();
+        
+        //entryPoint[0], all options
+        v.setQue(s.intersection1.entryPoint[0]);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.NORTH);
+        r.setValue(1);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.EAST);
+        r.setValue(2);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.SOUTH);
+        r.setValue(3);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.NORTH);
+        
+        //entryPoint[1], all options
+        v.setQue(s.intersection1.entryPoint[1]);
+        r.setValue(0);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.WEST);
+        r.setValue(1);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.EAST);
+        r.setValue(2);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.SOUTH);
+        r.setValue(3);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.WEST);
+        
+        //entryPoint[2], all options
+        v.setQue(s.intersection1.entryPoint[2]);
+        r.setValue(0);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.NORTH);
+        r.setValue(1);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.WEST);
+        r.setValue(2);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.SOUTH);
+        r.setValue(3);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.WEST);
+        
+        //entryPoint[3], all options
+        v.setQue(s.intersection1.entryPoint[3]);
+        r.setValue(0);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.NORTH);
+        r.setValue(1);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.EAST);
+        r.setValue(2);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.WEST);
+        r.setValue(3);
+        s.setPath(v);
+        assertEquals(v.getPath(), Direction.NORTH);
+        
+    }
+    
+    @Test
+    public void testPlaceVehicle() {
+        FakeRandom r = new FakeRandom(0);
+        s = new Simulation(100, 100000, 300, r);
+        Vehicle v = new Car();
+        
+        //test empty list
+        assertEquals(s.intersection1.entryPoint[0].size(), 0);
+        assertEquals(s.intersection1.entryPoint[1].size(), 0);
+        assertEquals(s.intersection1.entryPoint[2].size(), 0);
+        assertEquals(s.intersection1.entryPoint[3].size(), 0);
+        
+        //test adding one to an empty list
+        r.setValue(0);
+        s.placeVehicle(v);
+        r.setValue(1);
+        s.placeVehicle(v);
+        r.setValue(2);
+        s.placeVehicle(v);
+        r.setValue(3);
+        s.placeVehicle(v);
+        
+        assertEquals(s.intersection1.entryPoint[0].size(), 1);
+        assertEquals(s.intersection1.entryPoint[1].size(), 1);
+        assertEquals(s.intersection1.entryPoint[2].size(), 1);
+        assertEquals(s.intersection1.entryPoint[3].size(), 1);
+        
+        //test adding one to an occupied list
+        r.setValue(0);
+        s.placeVehicle(v);
+        r.setValue(1);
+        s.placeVehicle(v);
+        r.setValue(2);
+        s.placeVehicle(v);
+        r.setValue(3);
+        s.placeVehicle(v);
+        
+        assertEquals(s.intersection1.entryPoint[0].size(), 2);
+        assertEquals(s.intersection1.entryPoint[1].size(), 2);
+        assertEquals(s.intersection1.entryPoint[2].size(), 2);
+        assertEquals(s.intersection1.entryPoint[3].size(), 2);
+        
+        //test adding one with the default case
+        r.setValue(4);
+        s.placeVehicle(v);
+        assertEquals(s.intersection1.entryPoint[0].size(), 3);
+    }
+    
+    @Test
+    public void testAddVehicle() {
+        FakeRandom r = new FakeRandom(0);
+        s = new Simulation(100, 100000, 300, r);
+        
+        Vehicle v = s.addVehicle(true);
+        assertEquals(v instanceof Car, true);
+        assertEquals(v.getColor(), Color.red);
+        v = s.addVehicle(false);
+        assertEquals(v instanceof Car, true);
+        assertEquals(v.getColor(), Color.blue);
+        
+        r.setValue(1);
+        v = s.addVehicle(true);
+        assertEquals(v instanceof UtilityVehicle, true);
+        assertEquals(v.getColor(), Color.red);
+        v = s.addVehicle(false);
+        assertEquals(v instanceof UtilityVehicle, true);
+        assertEquals(v.getColor(), Color.orange);
+        
+        r.setValue(2);
+        v = s.addVehicle(true);
+        assertEquals(v instanceof SemiTruck, true);
+        assertEquals(v.getColor(), Color.red);
+        v = s.addVehicle(false);
+        assertEquals(v instanceof SemiTruck, true);
+        assertEquals(v.getColor(), Color.green);
+        
+        r.setValue(3);
+        v = s.addVehicle(true);
+        assertEquals(v instanceof Car, true);
+        assertEquals(v.getColor(), Color.red);
+        v = s.addVehicle(false);
+        assertEquals(v instanceof Car, true);
+        assertEquals(v.getColor(), Color.blue);
+    }
 }
 
